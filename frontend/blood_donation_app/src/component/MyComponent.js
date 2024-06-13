54df93055cca30bc2489c8ada30b8dbccac37a03
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2024-06-13 21:00:49 +0530

    component created

---
+++ frontend/blood_donation_app/src/component/MyComponent.js
@@ -0,0 +1,12 @@
+// src/MyComponent.js
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Hello, this is my component!</h1>
+    </div>
+  );
+}
+
+export default MyComponent;
