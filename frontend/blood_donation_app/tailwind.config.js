089be5e525f32be2985422870914dd5c26d8f1ad
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2024-06-13 21:27:26 +0530

    not workiung

---
+++ frontend/blood_donation_app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+
+module.exports = withMT({
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
+
