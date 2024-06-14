d62004392a5d55c7dd8536178711a5d321169411
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2024-06-14 07:54:22 +0530

    ss

---
+++ frontend/blood_donation_app/src/component/MyComponent.js
@@ -0,0 +1,15 @@
+// MyComponent.js
+import React from 'react';
+import { Button } from "@material-tailwind/react";
+
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Hello, this is my component!</h1>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default MyComponent;
