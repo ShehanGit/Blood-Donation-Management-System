33e7e7587311d6385e7d8fcc1371b51dadf89e8b
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2024-06-18 17:42:32 +0530

    ss

--- frontend/blood_donation_app/src/component/NavBar.jsx
+++ frontend/blood_donation_app/src/component/NavBar.jsx
@@ -5,47 +5,7 @@ import myImage from '../images/Logo.png';
 const NavBar = () => {
     return (
       <div>
-        <Navbar
-          fluid
-          rounded
-          style={{ height: '80px', fontSize: '1.25rem' }}
-        >
-          <Navbar.Brand href="https://flowbite-react.com">
-            <img
-              src={myImage}
-              className="mr-3"
-              style={{ height: '50px' }}
-              alt="Logo"
-            />
-            <span
-              className="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-              style={{ fontSize: '1.5rem' }}
-            >
-              Flowbite React
-            </span>
-          </Navbar.Brand>
-          <div className="flex md:order-2">
-            <Button >Get started</Button>
-            <Navbar.Toggle />
-          </div>
-          <Navbar.Collapse style={{ padding: '1rem' }}>
-            <Navbar.Link href="#" active style={{ fontSize: '1.25rem' }}>
-              Home
-            </Navbar.Link>
-            <Navbar.Link href="#" style={{ fontSize: '1.25rem' }}>
-              About
-            </Navbar.Link>
-            <Navbar.Link href="#" style={{ fontSize: '1.25rem' }}>
-              Services
-            </Navbar.Link>
-            <Navbar.Link href="#" style={{ fontSize: '1.25rem' }}>
-              Pricing
-            </Navbar.Link>
-            <Navbar.Link href="#" style={{ fontSize: '1.25rem' }}>
-              Contact
-            </Navbar.Link>
-          </Navbar.Collapse>
-        </Navbar>
+      <h1>Hello</h1>
       </div>
     );
   };
