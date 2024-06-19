baabd42faed4d325c19aae3675b1e6d56f52a3f7
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2024-06-19 11:56:32 +0530

    Navigation bar add

--- frontend/blood_donation_app/src/component/NavBar.jsx
+++ frontend/blood_donation_app/src/component/NavBar.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Button } from "flowbite-react";
+import { Button, Navbar } from "flowbite-react";
+
 
 
 const NavBar = () => {
     return (
         <div>
-            <h1>Hello, this is MyComponent!</h1>
-            <p>This is a simple React component.</p>
-            <Button>Click me</Button>
+            <Navbar fluid rounded>
+      <Navbar.Brand href="https://flowbite-react.com">
+        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#">About</Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
 
         </div>
     );
