b00a04bae56e7c9335fb6ec02252bd84a80e4d17
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2024-07-08 18:27:03 +0530

    ss

---
+++ backend/src/main/java/com/blood_donation_system/backend/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.blood_donation_system.backend.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and();
+
+        return http.build();
+    }
+}
